57cb58f7f18cb370b5daa051409d6cfeac230faa
Nodreame <nodreame94@gmail.com>
2020-09-20 01:10:34 +0800

    feat(dev): finish auto-generate problems files

--- dev/generateProblemsFile.js
+++ dev/generateProblemsFile.js
@@ -4,22 +4,18 @@ const fs = require('fs')
 
 async function main() {
   const problems = await getLeetcodeProblems()
+  
   const cacheFolderName = 'cache'
   const cacheFileName = 'problems'
   const [ isInit, genProblems ] = diff(problems, cacheFolderName, cacheFileName)
   if (genProblems.length > 0) {
     saveCacheProblemsSync(`./${cacheFolderName}/${cacheFileName}.json`, problems)
     console.log(`题目缓存文件${ isInit ? '初始化创建' : '全量更新' }完成`)
+    generateProblemFiles (genProblems, 'leetcode')
   }
 
-  // - [ ] 三. 文件生成
-  //  - [ ] 1. leetcode 文件夹确认
-  //  - [ ] 2. 读取包含占位字符的模板文件
-  //  - [ ] 3. 根据列表创建单个题目文件
-  //    - [ ] 使用 ${id}_${titleZh}.md 格式命名
-  //    - [ ] 复制模板作为文件内容
-  //    - [ ] 使用题目内容替换占位符
-  // generateProblemFiles (genProblems)
+  // TODO: 区分分割符，完善 Windows下体验
+  // TODO: 全流程异常处理
 }
 
 main()
@@ -132,4 +128,57 @@ function saveCacheProblemsSync (filename, problems) {
   fs.writeFileSync(filename, JSON.stringify(data));
 }
 
+/**
+ * 三. 文件生成
+ * @param {*} problems 
+ */
+function generateProblemFiles (problems, folderName) {
+  //  1. leetcode 文件夹确认
+  if(!fs.existsSync(folderName)) fs.mkdirSync(folderName)
+  //  2. 读取包含占位字符的模板文件
+  if(!fs.existsSync('template.md')) { console.log('TODO: 从 Github 上拉取，暂时略过') }
+  const tplContent = fs.readFileSync('template.md', 'utf8')
+  //  3. 需要获取现有 leetcode 文件夹下文件与 problems 做 diff (不能修改到已有的题目文件)
+  const existsFileNames = fs.readdirSync(`./${folderName}`)
+  const genProblems = getDiffProblemsWithExistsFiles(problems, existsFileNames)
+  //  4. 根据列表创建题目文件
+  //    - 使用 ${feId}_${titleZh}.md 格式命名
+  //    - 复制模板作为文件内容, 并使用题目内容替换占位符
+  const fileMap = new Map()
+  genProblems.forEach(item => {
+    const content = fillTpl(tplContent, item)
+    fileMap.set(`./leetcode/${item.feId}_${item.titleZh}.md`, content)
+  })
+  console.log(fileMap.size)
+  const keyArr = [...fileMap.keys()]
+  keyArr.forEach(k => {
+    fs.writeFile(k, fileMap.get(k), err => {
+      if (err) {
+        console.error(`${k} 生成出错: `, err)
+      } else {
+        console.log(`${k} 生成成功`)
+      }
+    })
+  })
+}
 
+// problems & 现有 leetcode 文件夹下文件 做 diff, 获取差异数组
+function getDiffProblemsWithExistsFiles (problems, existsFileNames) {
+  if (existsFileNames.length == 0) {
+    console.log('启动初始化 Leetcode 仓库生成逻辑')
+    return problems
+  }
+  const existsSet = [...existsFileNames.map(v => v.split('_')[0])]
+  const diffProblems = problems.filter(v => !existsSet.has(String(v.feId)))
+  console.log(`待生成问题列表 & 现有leetcode文件夹文件 对比完毕, 存在 ${diffProblems.length} 个新增问题. 具体如下所示:`)
+  console.log(diffProblems)
+  return diffProblems
+}
+
+// 替换模板字段
+function fillTpl(tpl, item) {
+  return tpl
+    .replace('{title}', item.titleZh)
+    .replace('{url}', `https://leetcode-cn.com/problems/${item.slug}/`)
+    .replace('{difficulty}', item.level === 1 ? 'Easy' : (item.level === 2 ? 'Medium' : 'Hard'))
+}
