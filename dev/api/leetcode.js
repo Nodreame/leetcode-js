e5322c652ff7322842864d584aabd956426826ac
Nodreame <nodreame94@gmail.com>
2020-09-19 21:32:38 +0800

    feat(dev): add problems cache

---
+++ dev/api/leetcode.js
@@ -0,0 +1,21 @@
+const axios = require('axios')
+
+function getProblems () {
+  // const problemsUrl = 'https://leetcode-cn.com/api/problems/algorithms/' // 旧链接，只有 1400 左右题目
+  const problemsUrl = 'https://leetcode-cn.com/api/problems/all/' // 新链接
+  return axios.get(problemsUrl)
+}
+
+function getTranslation () {
+  const translateParams = {
+    "operationName":"getQuestionTranslation",
+    "variables":{},
+    "query":"query getQuestionTranslation($lang: String) {\n  translations: allAppliedQuestionTranslations(lang: $lang) {\n    title\n    questionId\n    __typename\n  }\n}\n"
+  }
+  return axios.post('https://leetcode-cn.com/graphql', translateParams)
+}
+
+module.exports = {
+  getProblems,
+  getTranslation
+}
--- dev/generateProblemsFile.js
+++ dev/generateProblemsFile.js
@@ -1,55 +1,52 @@
-const axios = require('axios')
+// const axios = require('axios')
+const { getProblems, getTranslation } = require('./api/leetcode')
+const fs = require('fs')
 
 async function main() {
-  // 一. 获取题目列表
-  //  1.获取所有题目数据
-  // // const problemsUrl = 'https://leetcode-cn.com/api/problems/algorithms/' // 旧链接，只有 1400 左右题目
-  const problemsUrl = 'https://leetcode-cn.com/api/problems/all/'
-  const allProblemsResp = await axios.get(problemsUrl)
-  console.log(`获取所有题目数据${ allProblemsResp.status === 200 ? '成功': '失败' }`)
-  const problems = getProblems(allProblemsResp.data) // 成功获取列表，但是除了部分新增题目，其他 title 都是英文，需要翻译
-  // console.log(problems)
-  
-  //  2.获取翻译映射
-  const translateParams = {
-    "operationName":"getQuestionTranslation",
-    "variables":{},
-    "query":"query getQuestionTranslation($lang: String) {\n  translations: allAppliedQuestionTranslations(lang: $lang) {\n    title\n    questionId\n    __typename\n  }\n}\n"
+  const problems = await getLeetcodeProblems()
+  const cacheFolderName = 'cache'
+  const cacheFileName = 'problems'
+  const [ isInit, genProblems ] = diff(problems, cacheFolderName, cacheFileName)
+  if (genProblems.length > 0) {
+    saveCacheProblemsSync(`./${cacheFolderName}/${cacheFileName}.json`, problems)
+    console.log(`题目缓存文件${ isInit ? '初始化创建' : '全量更新' }完成`)
   }
-  const dictResp = await axios.post('https://leetcode-cn.com/graphql', translateParams)
-  console.log(`获取翻译映射${ dictResp.status === 200 ? '成功': '失败' }`)
-  const dictMap = getDict(dictResp.data)
-  
-  //  3.遍历完成翻译
-  const problemZhs = localizeProblems(problems, dictMap)
-  // console.log(problemZhs)
-  console.log('遍历完成翻译，结果长度为:', problemZhs.length)
 
-  // - [ ] 二. 初始化生成文件
-  //  - [ ] 1.创建 leetcode 文件夹
-  //  - [ ] 2.编写包含占位字符的模板文件 
-  //  - [ ] 3.创建单个题目文件
+  // - [ ] 三. 文件生成
+  //  - [ ] 1. leetcode 文件夹确认
+  //  - [ ] 2. 读取包含占位字符的模板文件
+  //  - [ ] 3. 根据列表创建单个题目文件
   //    - [ ] 使用 ${id}_${titleZh}.md 格式命名
   //    - [ ] 复制模板作为文件内容
   //    - [ ] 使用题目内容替换占位符
-  
-  // - [ ] 三. 考虑 Diff 逻辑, 用于出新题目后自动创建新题目文件
-  //   - [ ] 1.初次执行逻辑
-  //    - [ ] 缓存当前初始题目数据problems(原始数组太多冗余) & 翻译映射数组dictResp.data.translations（去掉无关属性）
-  //    - [ ] 创建 cache 文件夹, 以 JSON 对象形式缓存在 cache 文件夹中 (存储生成日期 & 数量 & 内容数组，数量用以对比)
-  //   - [ ] 2.更新 Diff 逻辑
-  //    - [ ] 待完成逻辑设计 
+  // generateProblemFiles (genProblems)
 }
 
 main()
 
+
 /**
- * get problems from json
- * @param {*} data 
+ * 一. 获取题目列表
  */
-function getProblems (data) {
-  if (!data || !data.stat_status_pairs) return []
-  return data.stat_status_pairs.map(item => {
+async function getLeetcodeProblems () {
+  //  1.获取所有题目数据
+  const problemsResp = await getProblems()
+  console.log(`获取题目数据${ problemsResp.status === 200 ? '成功': '失败' }`)
+  const problems = getSimplifyProblems(problemsResp.data.stat_status_pairs) // 获取简化列表
+  //  2.获取翻译映射（简化列表中许多 title 都是英文，需要参照映射表翻译）
+  const dictResp = await getTranslation()
+  console.log(`获取翻译映射${ dictResp.status === 200 ? '成功': '失败' }`)
+  const dictMap = getTranslationDict(dictResp.data.data.translations)
+  //  3.遍历完成翻译
+  const problemZhs = localizeProblems(problems, dictMap)
+  console.log('遍历完成翻译，结果长度为:', problemZhs.length)
+  return problemZhs
+}
+
+// get problems from json
+function getSimplifyProblems (data) {
+  if (!data) return []
+  return data.map(item => {
     return {
       id: item.stat.question_id,
       feId: item.stat.frontend_question_id,
@@ -61,24 +58,17 @@ function getProblems (data) {
   })
 }
 
-/**
- * get map from json
- * @param {*} data 
- */
-function getDict (data) {
+// get map from json
+function getTranslationDict (data) {
   const map = new Map()
-  if (!data || !data.translations) return map
-  data.translations.forEach(item => {
+  if (!data) return map
+  data.forEach(item => {
     map.set(item.questionId, item.title)
   })
   return map
 }
 
-/**
- * localize the array
- * @param {*} arr 
- * @param {*} map 
- */
+// localize the array
 function localizeProblems (arr=[], map) {
   if (!map) return arr
   return arr.map(item => {
@@ -89,3 +79,57 @@ function localizeProblems (arr=[], map) {
     }
   })
 }
+
+/**
+ * 二. 计算待生成文件列表
+ * @param {*} problems 
+ * @returns [ 初始化标志位, 待生成文件数组 ]
+ */
+function diff (problems, folderName, fileName) {
+  //  1. cache 文件夹校验（不存在则创建）
+  if (fs.existsSync(`./${folderName}`)) {
+    console.log(`缓存文件夹已存在`)
+  } else {
+    console.log(`缓存文件夹不存在, 进入初始化逻辑.`)
+    fs.mkdirSync(folderName)
+  }
+  //  2. 缓存文件校验
+  const problemsFileName = `./${folderName}/${fileName}.json`
+  if (fs.existsSync(problemsFileName)) {
+    console.log(`题目文件已存在, 开始运行 Diff 逻辑`)
+    // 获取 JSON 文件数据
+    const historyProblemsStr = fs.readFileSync(problemsFileName, 'utf8')
+    const historyProblemsObj = JSON.parse(historyProblemsStr)
+    console.log(`读取文件成功, 列表长度为 ${historyProblemsObj.length}`)
+    // Diff逻辑
+    if (historyProblemsObj.length === problems.length) {
+      console.log('题目列表长度不变，无需更新题目文件')
+      return [false, []]
+    } else {
+      // 抽取编号数组逐个比对, 获得新增题目列表
+      const tmpSet = new Set()
+      historyProblemsObj.problems.forEach(item => { tmpSet.add(item.id) })
+      const diffProblems = problems.filter(item => !tmpSet.has(item.id) )
+      if (diffProblems.length > 0) {
+        console.log(`Diff 执行完成, 共新增${diffProblems.length}道题目.`)
+        return [false, diffProblems]
+      }
+      return diffProblems
+    }
+  } else {
+    // 没有 problems.json 就创建并写数据
+    console.log(`题目缓存文件不存在, 进入初始化项目逻辑，共计${problems.length}道题目.`)
+    return [true, problems]
+  }
+}
+// 保存题目列表到缓存
+function saveCacheProblemsSync (filename, problems) {
+  const data = {
+    length: problems.length,
+    updatetime: new Date(),
+    problems: problems,
+  }
+  fs.writeFileSync(filename, JSON.stringify(data));
+}
+
+
